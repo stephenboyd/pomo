f318f93a8058f1e035fc7477e039e3c35d11a3ab
Stephen Boyd <stephen@sboyd.co>
2018-04-04 23:11:47 -0700

    Set up mock localStorage for tests

---
+++ src/setupTests.js
@@ -0,0 +1,24 @@
+// This snippet is mostly from https://stackoverflow.com/questions/32911630/how-do-i-deal-with-localstorage-in-jest-tests
+class MockLocalStorage {
+    constructor() {
+        this.store = {};
+    }
+
+    clear() {
+        this.store = {};
+    }
+
+    getItem(key) {
+        return this.store[key] || null;
+    }
+
+    setItem(key, value) {
+        this.store[key] = value.toString();
+    }
+
+    removeItem(key) {
+        delete this.store[key];
+    }
+};
+
+global.localStorage = MockLocalStorage;
